25f794020aaf223991d45bf907ab569490cb22bf
Joshua Godi <jgodi@bullhorn.com>
2016-01-06 13:09:54 -0600

    feature(branchList): Adding a get branch list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-
 var Git = require('./git');
 
 var ChildProcess = require('child_process');
 var Buffer = require('buffer').Buffer;
 
 module.exports = function (baseDir) {
-    return new Git(baseDir || process.cwd(), ChildProcess, Buffer);
+  return new Git(baseDir || process.cwd(), ChildProcess, Buffer);
 };
 
