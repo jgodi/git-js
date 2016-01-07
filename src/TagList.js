eaef817a82ba67e6c101c0b975c7b1eca9c97dc7
Joshua Godi <jgodi@bullhorn.com>
2016-01-07 09:45:50 -0600

    chore(tagList): Updating tag list parse to remove blank ones and sort correctly

--- src/TagList.js
+++ src/TagList.js
@@ -1,5 +1,3 @@
-var semver = require('semver');
-
 module.exports = TagList;
 
 function TagList(tagList) {
@@ -9,10 +7,39 @@ function TagList(tagList) {
 
 TagList.parse = function (tags) {
   if (tags.length) {
-    return new TagList(tags.split('\n').filter(function (tag) {
-      return tag.length;
-    }).sort(semver.compare));
+    return new TagList(tags.split('\n').filter(function (item) {
+      return item.length;
+    }).map(function (item) {
+      return item.trim();
+    }).sort(compVersions));
   } else {
     return new TagList(null);
   }
 };
+
+function compVersions(strV1, strV2) {
+  var nRes = 0
+    , parts1 = strV1.split('.')
+    , parts2 = strV2.split('.')
+    , nLen = Math.max(parts1.length, parts2.length);
+
+  for (var i = 0; i < nLen; i++) {
+    var nP1 = (i < parts1.length) ? parseInt(parts1[i], 10) : 0
+      , nP2 = (i < parts2.length) ? parseInt(parts2[i], 10) : 0;
+
+    if (isNaN(nP1)) {
+      nP1 = 0;
+    }
+    if (isNaN(nP2)) {
+      nP2 = 0;
+    }
+
+    if (nP1 != nP2) {
+      nRes = (nP1 > nP2) ? 1 : -1;
+      break;
+    }
+  }
+
+  return nRes;
+}
+
